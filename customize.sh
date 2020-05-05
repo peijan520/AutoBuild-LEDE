0ecc8418ee110235f0fb7bb9282e71dfa240a002
peijan520 <64638680+peijan520@users.noreply.github.com>
2020-05-05 22:11:53 +0800

    Update customize.sh

--- customize.sh
+++ customize.sh
@@ -5,5 +5,11 @@
 # Author: eSirPlayground
 # Youtube Channel: https://goo.gl/fvkdwm 
 #=================================================
-#1. Modify default IP
+#修改默认IP
 sed -i 's/192.168.1.1/192.168.20.1/g' openwrt/package/base-files/files/bin/config_generate
+#删除原主题
+#rm -rf package/lean/luci-theme-argon
+#取消bootstrap为默认主题
+#sed -i '/set luci.main.mediaurlbase=\/luci-static\/bootstrap/d' feeds/luci/themes/luci-theme-bootstrap/root/etc/uci-defaults/30_luci-theme-bootstrap
+#删除默认密码
+#sed -i "/CYXluq4wUazHjmCDBCqXF/d" package/lean/default-settings/files/zzz-default-settings
