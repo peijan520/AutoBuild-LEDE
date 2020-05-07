dbf75ec151016f7d6455aa00c7bb02965047ccf9
peijan520 <64638680+peijan520@users.noreply.github.com>
2020-05-07 08:31:16 +0800

    Update customize.sh

--- customize.sh
+++ customize.sh
@@ -10,6 +10,6 @@ sed -i 's/192.168.1.1/192.168.20.1/g' openwrt/package/base-files/files/bin/confi
 #删除原主题
 #rm -rf package/lean/luci-theme-argon
 #取消bootstrap为默认主题
-#sed -i '/set luci.main.mediaurlbase=\/luci-static\/bootstrap/d' feeds/luci/themes/luci-theme-bootstrap/root/etc/uci-defaults/30_luci-theme-bootstrap
+sed -i '/set luci.main.mediaurlbase=\/luci-static\/bootstrap/d' feeds/luci/themes/luci-theme-bootstrap/root/etc/uci-defaults/30_luci-theme-bootstrap
 #删除默认密码
-#sed -i "/CYXluq4wUazHjmCDBCqXF/d" package/lean/default-settings/files/zzz-default-settings
+sed -i "/CYXluq4wUazHjmCDBCqXF/d" package/lean/default-settings/files/zzz-default-settings
