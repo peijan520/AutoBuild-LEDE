920edc2a6e1566e676728d0c1ca9d951fb4f0930
peijan520 <64638680+peijan520@users.noreply.github.com>
2020-06-30 20:59:32 +0800

    Update customize.sh

--- customize.sh
+++ customize.sh
@@ -13,3 +13,4 @@ sed -i 's/192.168.1.1/192.168.20.1/g' openwrt/package/base-files/files/bin/confi
 #sed -i '/set luci.main.mediaurlbase=\/luci-static\/bootstrap/d' feeds/luci/themes/luci-theme-bootstrap/root/etc/uci-defaults/30_luci-theme-bootstrap
 #删除默认密码
 #sed -i "/CYXluq4wUazHjmCDBCqXF/d" package/lean/default-settings/files/zzz-default-settings
+#https://github.com/fw876/helloworld
